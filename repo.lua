5f6359ef934974613914f4f758856dd4e57f926b
Diogo Silva <86174715+Diogo-ss@users.noreply.github.com>
2024-05-10 17:21:56 -0300

    Update repo.lua

--- repo.lua
+++ repo.lua
@@ -1,24 +1,13 @@
 manteiners = { "Diogo-ss" }
+os = "linux"
+arch = "x86"
 
--- env = {
---   user = "loopkg",
---   repo = "core-spec",
---   branch = "main",
--- }
-
--- the only two values allowed are `pkg.name` and `pkg.version`
-url = "https://raw.githubusercontent.com/pkger/core-pkgs/main/pkgs/${{ name }}/${{ version }}/pkg.lua"
-
--- Get the complete repo tree.
--- For now only compatible with GitHub and API format
--- TODO: GitLab
+-- only two values allowed are `pkg.name` and `pkg.version`
+url = "https://raw.githubusercontent.com/pkger/core-pkgs/main/pkgs/${{ pkg.name }}/${{ pkg.version }}/pkg.lua"
 
 search = {
    type = "github",
    url = "https://api.github.com/repos/pkger/core-pkgs/git/trees/main?recursive=1",
 }
 
--- search = {
---   type = "api",
---   url = "https://api.pkger.dev/pkgs/?search=${{ pkg.name }}",
--- }
+-- TODO: GitLab, API, metadata
