5a2c641a4558830b235933b112b27508b5038c02
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-05-16 12:35:49 -0300

    fix: neovim

--- pkgs/neovim/0.9.4/pkg.lua
+++ pkgs/neovim/0.9.4/pkg.lua
@@ -6,12 +6,14 @@ manteiners = "Diogo-ss"
 version = "0.9.4"
 url = "https://github.com/neovim/neovim/releases/download/v${{ version }}/nvim-linux64.tar.gz"
 
-remove_source_dir = false
-
-bin = "nvim-linux64/bin/nvim"
+bin = "bin/nvim"
 
 checkver = {
 	url = "https://api.github.com/repos/neovim/neovim/releases/latest",
 	jsonpath = "tag_name",
 	regex = "[Vv]?(.+)",
 }
+
+function install()
+	system("mv * ..")
+end
