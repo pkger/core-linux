9ee5fecae2484eeb778ae1507926d5796c813e95
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-05-16 12:43:17 -0300

    feat: add 0.9.5

---
+++ pkgs/neovim/0.9.5/pkg.lua
@@ -0,0 +1,21 @@
+name = "neovim"
+description = "Vim-fork focused on extensibility and usability"
+homepage = "https://neovim.io"
+license = "Apache-2.0"
+manteiners = "Diogo-ss"
+version = "0.9.5"
+url = "https://github.com/neovim/neovim/releases/download/v${{ version }}/nvim-linux64.tar.gz"
+
+bin = "bin/nvim"
+
+keep_source_file = true
+
+checkver = {
+	url = "https://api.github.com/repos/neovim/neovim/releases/latest",
+	jsonpath = "tag_name",
+	regex = "[Vv]?(.+)",
+}
+
+function install()
+	system("mv * ..")
+end
